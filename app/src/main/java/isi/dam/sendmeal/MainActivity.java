73f15308839c72d62c602290ae148e25759b5e31
HerniiGoH <calbornoz@frsf.utn.edu.ar>
2019-08-30 02:31:15 -0300

    "Interfaces Version 4: Radio Group ahora modifica el Credito Inicial"

--- app/src/main/java/isi/dam/sendmeal/MainActivity.java
+++ app/src/main/java/isi/dam/sendmeal/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CompoundButton;
+import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -20,9 +21,10 @@ public class MainActivity extends AppCompatActivity {
 
         final DecimalFormat formatoMonto = new DecimalFormat("$0.00");
 
-        SeekBar credito = findViewById(R.id.IngresoCredito);
+        final SeekBar credito = findViewById(R.id.IngresoCredito);
         Switch esVendedor = findViewById(R.id.ButtonEsVendedor);
         final TextView montoCredito = findViewById(R.id.MontoCredito);
+        RadioGroup grupo = findViewById(R.id.GrupoBotones);
 
         montoCredito.setText(formatoMonto.format(0));
 
@@ -42,7 +44,7 @@ public class MainActivity extends AppCompatActivity {
         credito.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser){
-                int val = progress;//(progress * (seekBar.getWidth() - 2 * seekBar.getThumbOffset())) / seekBar.getMax();
+                int val = progress;
                 montoCredito.setText(formatoMonto.format(val));
                 montoCredito.setX(seekBar.getThumb().getBounds().left - val/15);
             }
@@ -52,6 +54,17 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+        grupo.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                switch (checkedId){
+                    case R.id.ButtonBase: credito.setProgress(100); break;
+                    case R.id.ButtonPremium: credito.setProgress(250); break;
+                    case R.id.ButtonFull: credito.setProgress(500); break;
+                }
+            }
         });
 
     }
