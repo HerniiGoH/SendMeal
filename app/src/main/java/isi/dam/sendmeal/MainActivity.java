6a74fc2997a271da012309d7a4bb00649dd5f0e8
HerniiGoH <calbornoz@frsf.utn.edu.ar>
2019-08-29 22:02:21 -0300

    "Interfaces Version 1"

--- app/src/main/java/isi/dam/sendmeal/MainActivity.java
+++ app/src/main/java/isi/dam/sendmeal/MainActivity.java
@@ -3,6 +3,9 @@ package isi.dam.sendmeal;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Switch esVendedor = findViewById(R.id.ButtonEsVendedor);
+
+        esVendedor.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
+                if(isChecked){
+                    findViewById(R.id.LayoutDatosBancarios).setVisibility(View.VISIBLE);
+                }
+                else {
+                    findViewById(R.id.LayoutDatosBancarios).setVisibility(View.GONE);
+                }
+            }
+
+        });
+
     }
+
 }
