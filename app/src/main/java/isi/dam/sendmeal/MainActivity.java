3702c8602fc39d010fe0de90d3fabd86e035cdc3
HerniiGoH <calbornoz@frsf.utn.edu.ar>
2019-08-30 14:08:06 -0300

    "Interfaces Version 6: Segunda versión de la Fecha de la Tarjeta"

--- app/src/main/java/isi/dam/sendmeal/MainActivity.java
+++ app/src/main/java/isi/dam/sendmeal/MainActivity.java
@@ -27,68 +27,8 @@ public class MainActivity extends AppCompatActivity {
         Switch esVendedor = findViewById(R.id.ButtonEsVendedor);
         final TextView montoCredito = findViewById(R.id.MontoCredito);
         RadioGroup grupo = findViewById(R.id.GrupoBotones);
-        final EditText date = findViewById(R.id.IngresoFechaTarjeta);
 
         final DecimalFormat formatoMonto = new DecimalFormat("$0.00");
-
-        TextWatcher tw = new TextWatcher() {
-            private String current = "";
-            private String ddmmyyyy = "MMYYYY";
-            private Calendar cal = Calendar.getInstance();
-
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (!s.toString().equals(current)) {
-                    String clean = s.toString().replaceAll("[^\\d.]|\\.", "");
-                    String cleanC = current.replaceAll("[^\\d.]|\\.", "");
-
-                    int cl = clean.length();
-                    int sel = cl;
-                    for (int i = 0; i <= cl && i < 4; i += 2) {
-                        sel++;
-                    }
-                    //Fix for pressing delete next to a forward slash
-                    if (clean.equals(cleanC)) sel--;
-
-                    if (clean.length() < 6){
-                        clean = clean + ddmmyyyy.substring(clean.length());
-                    }else{
-                        //This part makes sure that when we finish entering numbers
-                        //the date is correct, fixing it otherwise
-                        int mon  = Integer.parseInt(clean.substring(0,2));
-                        int year = Integer.parseInt(clean.substring(2,6));
-
-                        mon = mon < 1 ? 1 : mon > 12 ? 12 : mon;
-                        cal.set(Calendar.MONTH, mon-1);
-                        year = (year<2018)?2018:(year>2100)?2100:year;
-                        cal.set(Calendar.YEAR, year);
-                        // ^ first set year for the line below to work correctly
-                        //with leap years - otherwise, date e.g. 29/02/2012
-                        //would be automatically corrected to 28/02/2012
-                        clean = String.format("%02d%02d", mon, year);
-                    }
-
-                    clean = String.format("%s/%s", clean.substring(0, 2),
-                            clean.substring(2, 6));
-
-                    sel = sel < 0 ? 0 : sel;
-                    current = clean;
-                    date.setText(current);
-                    date.setSelection(sel < current.length() ? sel : current.length());
-                }}
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
-        };
-
-        date.addTextChangedListener(tw);
 
         montoCredito.setText(formatoMonto.format(0));
 
