8ed59760700dc0fb58f16d3fef84c925eca89d97
juano187 <jmauro@frsf.utn.edu.ar>
2019-09-19 20:34:50 -0300

    "intento de push, boton atras "arreglado" en crear plato"

--- app/src/main/java/isi/dam/sendmeal/CrearItem.java
+++ app/src/main/java/isi/dam/sendmeal/CrearItem.java
@@ -3,6 +3,7 @@ package isi.dam.sendmeal;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -110,5 +111,11 @@ public class CrearItem extends AppCompatActivity {
     public boolean onSupportNavigateUp(){
         onBackPressed();
         return true;
+    }
+    @Override
+    public void onBackPressed()
+    {
+        Intent i3 = new Intent(this, ListaItems.class);
+        startActivity(i3);
     }
 }
