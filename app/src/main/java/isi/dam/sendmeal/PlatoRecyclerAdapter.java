edac028835df5c432574f30971ec4464f75674f3
HerniiGoH <calbornoz@frsf.utn.edu.ar>
2019-09-26 19:53:09 -0300

    "Merge con Lab02"

--- app/src/main/java/isi/dam/sendmeal/PlatoRecyclerAdapter.java
+++ app/src/main/java/isi/dam/sendmeal/PlatoRecyclerAdapter.java
@@ -1,22 +1,16 @@
+
 package isi.dam.sendmeal;
 
 import android.content.Context;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.widget.ImageView;
-import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
-import androidx.fragment.app.DialogFragment;
-import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.RecyclerView;
-
-import org.w3c.dom.Text;
 
 import java.util.List;
 
