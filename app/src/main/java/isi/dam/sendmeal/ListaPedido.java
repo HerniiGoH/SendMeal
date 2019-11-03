2a5fd93c66342fa93f04a0382a25eab839088b60
HerniiGoH <calbornoz@frsf.utn.edu.ar>
2019-11-03 15:14:02 -0300

    "Pedido v1.1"

--- app/src/main/java/isi/dam/sendmeal/ListaPedido.java
+++ app/src/main/java/isi/dam/sendmeal/ListaPedido.java
@@ -1,6 +1,55 @@
 package isi.dam.sendmeal;
 
-public class ListaPedido {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class ListaPedido extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    Toolbar toolbar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pedidos);
+        toolbar = findViewById(R.id.toolbar_crear_item);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                finish();
+            }
+        });
+
+
+        mRecyclerView = findViewById(R.id.rvItemsPedido);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        FloatingActionButton fab = findViewById(R.id.agregar_flotante);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i2 = new Intent(ListaPedido.this, ListaItemsPedido.class);
+                startActivity(i2);
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        onBackPressed();
+        return true;
+    }
 
 }
